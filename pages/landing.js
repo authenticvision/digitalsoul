02ef5b2cf022ff64dd25ed6a753b6e5d7422fa7f
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-10-21 12:47:01 -0300

    landing: don't render or poll if there's an error

--- pages/landing.js
+++ pages/landing.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useRouter } from 'next/router'
 import NextHead from 'next/head.js'
-//import paseto, { V4 as passetoquatro } from 'paseto'
+import { wait, poll } from '@/lib/landing/utils'
 
 import { useSession } from 'next-auth/react'
 import { getServerSession } from 'next-auth/next'
@@ -9,11 +9,15 @@ import prisma from "@/lib/prisma"
 import MetaAnchor from '@/lib/api.metaanchor.io'
 import { auth } from "auth"
 
-import { Layout, Alert, Button } from '@/components/ui'
-import { NFTCard } from '@/components/landing'
+import { Alert, Button } from '@/components/ui'
+import { Layout, OwnerCardView, ReceivingCardView, LostCardView } from '@/components/landing'
+
+const OWNER_STATUS = 'owner'
+const RECEIVING_STATUS = 'receiving'
+const LOST_STATUS = 'lost'
 
 export async function getServerSideProps(context) {
-	// TODO: Move most of this stuff into it's own module
+	// TODO: Maybe move most of this stuff into it's own module
 	let nft = null
 	let errorMsg = null
 	let assetData = null
@@ -45,33 +49,19 @@ export async function getServerSideProps(context) {
 		if (!wallet) {
 			errorMsg = 'This wallet does not exist on our records!'
 		} else {
-			// XXX: This endpoint is broken for the test sip token
 			try {
 				const { data: assetResponse, status } = await api.getAssetBySip(av_sip)
 
-				console.log("Asset Response: ", assetResponse, status)
+				assetData = assetResponse
 			} catch (error) {
 				console.error(error)
-				// XXX: Ignore until the API is fixed
-				// errorMsg = 'Error while trying to fetch API'
-			}
-
-			// XXX: Where do we extract this public key?
-			//const publicKey = 'k4.public.f2AxH__c3AQy_abwIYAZvwzLYrLPAUNH5o6cFzPj1_0'
-			//const payload = await paseto.V4.verify(av_sip, publicKey)
-			//console.log(payload, data)
-
-			assetData = {
-				"token_uri": "http://localhost:3000/api/v1/collection/0xaacb9848a60cc6af9da43fe5a57bfab90b910b2dbe9ff87243f43898147a5dd4",
-				"owner": "0x3B7e66cef9ae94247793Afa638C9828Fb2A83C54",
-				"token_id": "52",
-				"anchor": "0x9e0a245aa649eaf595872c7d221241365ccdc2c198cc1e3801b5c8de0d819f36"
+				errorMsg = 'Error while trying to fetch API'
 			}
 
 			// TODO: Sanitize and validate the assetData.anchor is a valid
 			// anchor, raw queries are sanitized by default
 			const nftResults = await prisma.$queryRaw`
-				SELECT slid, anchor, contracts.address AS contract_address,
+				SELECT slid, metadata, anchor, contracts.address AS contract_address,
 				contracts.name AS contract_name, contracts.csn AS contract_csn
 				FROM nfts
 				INNER JOIN contracts ON contracts.id = nfts.contract_id
@@ -83,14 +73,12 @@ export async function getServerSideProps(context) {
 			if (nftResults.length == 1) {
 				nft = nftResults[0]
 			} else {
-				errorMsg = 'There are two nfts locally, database is broken'
+				errorMsg = 'There are two NFTs locally, database is broken'
 			}
 		}
 	} else {
 		noData = true
 	}
-
-	console.log(nft);
 
 	return {
 		props: {
@@ -99,15 +87,101 @@ export async function getServerSideProps(context) {
 			assetData,
 			noData,
 			errorMsg,
+			avSip: av_sip,
 			session: JSON.parse(JSON.stringify(session)) // XXX: NextJS is dumb
 		}
 	}
 }
 
-const LandingNFT = (props) => {
-	const [error, setError] = useState(props.errorMsg)
-	const [nft, setNFT] = useState(props.nft)
-	const [assetData, setAssetData] = useState(props.assetData)
+const LandingNFT = ({ nft, noData, avSip, errorMsg, wallet, assetData, props }) => {
+	const [error, setError] = useState(errorMsg)
+	const [newOwner, setNewOwner] = useState()
+	const [ownershipStatus, setOwnershipStatus] = useState(
+		assetData.owner == wallet.address ? OWNER_STATUS : RECEIVING_STATUS
+	)
+
+	const CurrentCard = useCallback(() => {
+		switch(ownershipStatus) {
+			case OWNER_STATUS:
+				return (<OwnerCardView wallet={wallet} assetData={assetData} nft={nft} />)
+			case RECEIVING_STATUS:
+				return (<ReceivingCardView wallet={wallet} assetData={assetData} nft={nft} />)
+			case LOST_STATUS:
+				return (<LostCardView wallet={wallet} newOwner={newOwner} nft={nft} />)
+			default:
+				return (<ReceivingCardView wallet={wallet} assetData={assetData} nft={nft} />)
+		}
+	}, [ownershipStatus, nft, wallet, assetData, newOwner])
+
+	const nftWasClaimed = (result) => {
+		if (wallet.address == result.owner) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+	const nftWasTransfered = (result) => {
+		if (wallet.address != result.owner) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+	const verifySipToken = async() => {
+		const response = await fetch('/api/internal/landing/verify', {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				avSip
+			})
+		})
+
+		return response.json()
+	}
+
+	const claimNFT = async () => {
+		const response = await fetch('/api/internal/landing/claim', {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				avSip,
+				address: wallet.address
+			})
+		})
+
+		const data = response.json()
+
+		if (response.ok) {
+			await poll(verifySipToken, nftWasClaimed, 3000)
+
+			setOwnershipStatus(OWNER_STATUS)
+		} else {
+			setError('Something went wrong when trying to claim NFT')
+		}
+	}
+
+	const checkNFTOwnership = async () => {
+		const result = await poll(verifySipToken, nftWasTransfered, 3000)
+
+		setNewOwner(result.owner)
+		setOwnershipStatus(LOST_STATUS)
+	}
+
+	useEffect(() => {
+		if (!error) {
+			if (ownershipStatus == RECEIVING_STATUS) {
+				claimNFT()
+			} else if (ownershipStatus == OWNER_STATUS) {
+				checkNFTOwnership()
+			}
+		}
+	}, [ownershipStatus])
 
 	return (
 		<Layout>
@@ -123,9 +197,11 @@ const LandingNFT = (props) => {
 						)}
 					</div>
 
-					<div className="flex justify-center py-2">
-						<NFTCard assetData={assetData} nft={nft} wallet={props.wallet} />
-					</div>
+					{!error && (
+						<div className="flex justify-center py-2 flex-col">
+							<CurrentCard nft={nft} assetData={assetData} wallet={wallet} />
+						</div>
+					)}
 				</main>
 			</div>
 		</Layout>
