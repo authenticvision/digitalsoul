e86bd268bf8ac14e8e775859e95439709479a179
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-04 22:37:56 -0300

    components/studio: rework NFT Image Uploader

--- components/studio/NFTImageUploader.js
+++ components/studio/NFTImageUploader.js
@@ -4,11 +4,26 @@ import { FilePond, registerPlugin } from 'react-filepond'
 import 'filepond/dist/filepond.min.css'
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+
+registerPlugin(
+	FilePondPluginImageExifOrientation,
+	FilePondPluginFileValidateSize,
+	FilePondPluginImagePreview
+)
 
 const NFTImageUploader = ({ anchor, onFinish, ...props }) => {
 	const [assets, setAssets] = useState([])
+
+	const afterProcess = () => {
+		setAssets([])
+		onFinish()
+	}
+
+	const onCloseModal = (e) => {
+		setAssets([])
+	}
 
 	return (
 		<div className="modal-box">
@@ -17,8 +32,12 @@ const NFTImageUploader = ({ anchor, onFinish, ...props }) => {
 					files={assets}
 					onupdatefiles={setAssets}
 					allowMultiple={true}
+					allowRevert={false}
+					allowReorder={false}
+					credits={false}
 					maxFiles={3}
-					onprocessfiles={onFinish}
+					maxFileSize={'3MB'}
+					onprocessfiles={afterProcess}
 					server={`/api/internal/assets/${anchor}`}
 					name="assets"
 					labelIdle='Drag & Drop your NFT files or <span class="filepond--label-action">Browse</span>'
@@ -27,7 +46,7 @@ const NFTImageUploader = ({ anchor, onFinish, ...props }) => {
 
 			<div className="modal-action">
 				<form method="dialog">
-					<button className="btn">Cancel</button>
+					<button onClick={onCloseModal} className="btn">Cancel</button>
 				</form>
 			</div>
 		</div>
