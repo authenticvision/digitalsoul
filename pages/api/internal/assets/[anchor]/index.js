57a24b1744bc9993161e039adcafc5c2348f307a
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-05 00:20:22 -0300

    api/internal: split into multiple steps when processing new NFTs

--- pages/api/internal/assets/[anchor]/index.js
+++ pages/api/internal/assets/[anchor]/index.js
@@ -1,17 +1,51 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
 
-import crypto from 'crypto'
+import path from 'path'
+import mime from 'mime'
+
 import prisma from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
+
 import formidable from 'formidable-serverless'
+import { readFileSync, writeFileSync } from 'fs';
+import { keccak256, id } from 'ethers'
 
 const allowedMethods = ['POST']
+
+const allowedMimeTypes = [
+	mime.getType('jpg'),
+	mime.getType('gif'),
+	mime.getType('png'),
+	mime.getType('m4a'),
+	mime.getType('mp4'),
+	mime.getType('mkv'),
+	mime.getType('avi'),
+	mime.getType('webm'),
+	mime.getType('webp'),
+	mime.getType('mov'),
+	mime.getType('wmv')
+]
 
 export const config = {
   api: {
     bodyParser: false,
   }
+}
+
+const STORAGE_DIR = process.env.STORAGE_DIR
+
+const dropAssetsFromNFT = async (nft) => {
+	await prisma.NFT.update({
+		where: {
+			id: nft.id
+		},
+		data: {
+			assets: {
+				set: []
+			}
+		}
+	})
 }
 
 export default async function handle(req, res) {
@@ -26,10 +60,92 @@ export default async function handle(req, res) {
 	}
 
 	const { anchor } = req.query
+
+	const nft = await prisma.NFT.findFirst({
+		where: {
+			anchor
+		},
+		include: {
+			contract: true,
+			assets: true
+		}
+	})
+
+	// TODO: What to do with assets that aren't attached to any other NFT?
+	// Should this clean up run on an async job?
+	await dropAssetsFromNFT(nft)
+
 	const form = new formidable.IncomingForm()
 	form.parse(req, async (err, fields, files) => {
-		console.log(err, fields, files.assets)
+		const uploadedFile = files.assets
 
-		return res.status(202).json({})
-	});
+		if (!allowedMimeTypes.includes(uploadedFile.type)) {
+			return res.status(422).json({ message: 'File type is not allowed' })
+		}
+
+		const filePath = uploadedFile.path
+		const fileContent = readFileSync(filePath)
+
+		const hashedFile = keccak256(fileContent)
+		const finalHash = id(`${nft.contract.csn}:${hashedFile}`)
+
+		const existingAsset = await prisma.asset.findFirst({
+			where: {
+				assetHash: finalHash
+			}
+		})
+
+		if (existingAsset) {
+			await prisma.NFT.update({
+				where: {
+					id: nft.id
+				},
+				data: {
+					updatedAt: new Date(),
+					assets: {
+						connect: [{
+							id: existingAsset.id
+						}]
+					}
+				}
+			})
+
+			return res.json({
+				existingAsset
+			})
+		} else {
+			try {
+				writeFileSync(path.join(STORAGE_DIR, finalHash), fileContent)
+
+				const asset = await prisma.asset.create({
+					data: {
+						fileName: finalHash,
+						assetHash: finalHash,
+						assetType: 'image',
+						originalFileName: uploadedFile.name,
+						contract: {
+							connect: {
+								id: nft.contract.id
+							}
+						},
+						nfts: {
+							connect: [{
+								id: nft.id
+							}]
+						}
+					}
+				})
+
+				return res.json({
+					asset
+				})
+			} catch (error) {
+				console.error(error)
+
+				return res.status(500).json({
+					error: 'There was an error when storing the asset'
+				})
+			}
+		}
+	})
 }
