9e566920682464956d5473132fd1f915c4d7b38c
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-09 02:01:10 -0300

    components/studio: use NFTImageEdit on NFTView component

--- components/studio/NFTView.js
+++ components/studio/NFTView.js
@@ -1,27 +1,11 @@
 import React, { useRef } from 'react'
 import Link from 'next/link'
 import { formatAddress, generateAssetURL } from '@/lib/utils'
-import { TraitsBox, NFTImageUploader } from '@/components/studio'
-import editImg from '@/public/icons/edit.svg'
+import { TraitsBox, NFTImageEdit } from '@/components/studio'
 import Image from 'next/image'
 
 // TODO: Split this component into two, a standalone header & the body
 const NFTView = ({ nft, wallet, contract, onFinishEditing, ...props }) => {
-	const editingModal = useRef(null)
-
-	const onEdit = (event) => {
-		editingModal.current.showModal()
-	}
-
-	const onFinishUploading = (e) => {
-		editingModal.current.close()
-
-		onFinishEditing()
-	}
-
-	const hasAssets = nft.assets.length > 0
-	const assetURL  = hasAssets ? generateAssetURL(nft.assets[0].assetHash) : null
-
 	return (
 		<>
 			<div className="flex border-b border-raven-700">
@@ -50,24 +34,9 @@ const NFTView = ({ nft, wallet, contract, onFinishEditing, ...props }) => {
 			<div className="flex w-9/12 ml-8 mt-8 mb-8">
 				<div className="grid">
 					<div className="flex items-center justify-center flex-col lg:flex-row">
-						<div className="relative group">
-							<button type="button" onClick={onEdit} className="absolute z-10 invisible group-hover:visible right-5 top-5 border cursor-pointer hover:shadow-white border-white rounded-full hover:shadow-sm">
-								<Image src={editImg} height={35} width={35} />
-							</button>
-
-							<dialog ref={editingModal} id="nft-modal-editing" className="modal">
-								<NFTImageUploader anchor={nft.anchor} onFinish={onFinishUploading} />
-							</dialog>
-
-							<div className="relative w-[350px] h-[350px]">
-								{hasAssets ? (
-									<Image src={assetURL} fill className="object-cover max-w-sm rounded-lg shadow-2xl" />
-								) : (
-									<Image src="/nft-fallback-cover.webp" fill className="object-cover max-w-sm rounded-lg shadow-2xl" />
-								)}
-							</div>
-
-						</div>
+						<>
+							<NFTImageEdit nft={nft} onFinishEditing={onFinishEditing} />
+						</>
 
 						<div className="ml-8">
 							<h1 className="text-2xl text-gray-400">
