3b935c2ce8238896ab6ff3892d2c3c636bf9a26f
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-22 00:22:20 -0300

    schema: drop pluralized version from AssetNFT model

---
+++ prisma/migrations/20231122024035_drop_asset_type_from_assets_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `asset_type` on the `assets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "assets" DROP COLUMN "asset_type";
