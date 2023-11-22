1eafc7fea2a045a7e5ecf2347e3ec3ca1e902a5a
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-22 00:25:10 -0300

    assets: detect nfts primary asset using a tiny service

--- components/studio/NFTCard.js
+++ components/studio/NFTCard.js
@@ -1,4 +1,4 @@
-import { truncate, formatAddress, cn, generateAssetURL } from '@/lib/utils'
+import { truncate, formatAddress, cn, discoverPrimaryAsset, generateAssetURL } from '@/lib/utils'
 import Link from 'next/link'
 import Image from 'next/image'
 import NFTCaption from './NFTCaption'
@@ -11,16 +11,14 @@ const NFTCard = ({ nft, contractName, staticCaption, ...props }) => {
 		`
 	)
 
-	const hasAssets = nft.assets.length > 0
-	const assetURL  = hasAssets ? generateAssetURL(
-		nft.contract.csn, nft.assets[0].asset.assetHash
-	) : null
+	const primaryAsset = discoverPrimaryAsset(nft)
+	const assetURL  = primaryAsset ? generateAssetURL(nft.contract.csn, primaryAsset?.asset?.assetHash) : null
 
 	return (
 		<Link href={`/studio/${nft.contract.csn.toLowerCase()}/${nft.anchor}`}
 			  className={cardRootClassName}>
 			<figure className="w-[254px] h-[254px] object-cover relative">
-				{hasAssets? (
+				{primaryAsset ? (
 					<Image src={assetURL}
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 						fill alt={nft.metadata?.description} />
