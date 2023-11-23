b9b4f7197ceb38065b9f4a231ce709fc5744c3eb
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-11-22 23:41:47 -0300

    studio: add initial dummy assets page

---
+++ pages/studio/[csn]/assets.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react"
+import NextHead from 'next/head.js'
+
+import { AppLayout, Loading, ErrorPage } from '@/components/ui'
+
+import { AssetsList } from '@/components/studio'
+
+import { useSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth/next'
+import { auth } from 'auth'
+
+import prisma from '@/lib/prisma'
+
+export async function getServerSideProps(context) {
+	const session = await auth(context.req, context.res)
+	let nfts = []
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			}
+		}
+	}
+
+	const { csn } = context.query
+
+	const wallet = {
+		id: session.wallet.id,
+		address: session.wallet.address?.toLowerCase()
+	}
+
+	const contract = await prisma.contract.findFirst({
+		where: {
+			ownerId: wallet.id,
+			csn: {
+				equals: csn,
+				mode: 'insensitive'
+			}
+		},
+		select: {
+			id: true,
+			name: true,
+			csn: true,
+			address: true,
+			network: true
+		}
+	})
+
+	if (!contract) {
+		return {
+			props: {
+				forbidden: true
+			}
+		}
+	}
+
+	return {
+		props: {
+			wallet,
+			contract,
+			session: JSON.parse(JSON.stringify(session)) // XXX: NextJS is dumb
+		}
+	}
+}
+
+const Assets = ({ wallet, contract, ...props }) => {
+	const { data: session, status } = useSession({
+		required: true,
+		onUnauthenticated() {
+			Router.push('/')
+		}
+	})
+
+	if (props.forbidden) {
+		return (
+			<ErrorPage status={403} />
+		)
+	}
+
+	return (
+		<>
+			<NextHead>
+				<title>DigitalSoul - Studio - Assets</title>
+			</NextHead>
+			<AppLayout wallet={wallet} contractId={contract.id}>
+				<div className="page w-full ">
+					<main className="flex flex-col">
+						<AssetsList
+							contract={contract} />
+					</main>
+				</div>
+			</AppLayout>
+		</>
+	)
+}
+
+export default Assets
