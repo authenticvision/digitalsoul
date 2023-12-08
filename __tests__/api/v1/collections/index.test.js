a2a63ecd9900c77502082f4c2bf914f681cd3805
Thomas Bergmueller <thomas.bergmueller@authenticvision.com>
2023-12-08 18:57:38 +0100

    Allow variable-access to nft.private_data
    In metadata-variable replacement, add the nft's private_data as
    variables, which can be used like any other variable

--- __tests__/api/v1/collections/index.test.js
+++ __tests__/api/v1/collections/index.test.js
@@ -96,4 +96,73 @@ describe('/api/v1/collections/[csn]/[anchor]', () => {
 			some: "value"
 		})
 	})
+
+	it('responds with replaced nft.private_data variables', async () => {
+		const wallet = await ctx.db.wallet.create({
+			data: {
+				address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' // this is the first hardhat testing wallet
+			}
+		})
+
+		const contract = await ctx.db.contract.create({
+			data: {
+				csn: 'BEEF',
+				name: 'Deadbeef',
+				address: '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+				network: 'local-test',
+				settings: {
+					NFT_NAME: "DigitalSoul #[MY_PRIVATE_VALUE]", // Tests the contract-wise settings including Variables
+					NFT_DESCRIPTION: "Contract is deployed at [CONTRACT_ADDRESS] as '[COLLECTION_NAME]'" // 
+				},
+				owner: {
+					connect: {
+						id: wallet.id
+					}
+				}
+			}
+		})
+
+		const nft = await ctx.db.NFT.create({
+			data: {
+				slid: 'TEST',
+				anchor: '0x505def45449ab0da5a5d58456298c4e2634c698cccc30f6259e3c6695c664731',
+				metadata: {
+					"some": "value",
+					"attributes": [{
+						"type": "SuperFancy",
+						"value": "[MY_PRIVATE_VALUE]"
+					}]
+				},
+				privateData: {
+					"my_private_value": "002"
+				},
+				contract: {
+					connect: {
+						id: contract.id
+					}
+				}
+			}
+		})
+
+		const { req, res } = createMocks({
+			method: 'GET',
+			query: {
+				csn: 'BEEF',
+				anchor: '0x505def45449ab0da5a5d58456298c4e2634c698cccc30f6259e3c6695c664731'
+			}
+		})
+
+		await api(req, res)
+		const data = await res._getJSONData()
+
+		expect(data).toEqual({
+			name: 'DigitalSoul #002',
+			description: "Contract is deployed at 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF as 'Deadbeef'",
+			some: "value",
+			attributes: [{
+				type: "SuperFancy",
+				value: "002"
+			}]
+		})
+	})
 })
