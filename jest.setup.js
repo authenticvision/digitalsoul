b914bcc10ef2ae1c73f29a6d343039023b373056
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-12-12 19:45:53 -0300

    tests: mock singleton prisma instance for tests and drop custom helpers

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,10 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
+jest.mock("./lib/prisma", () => {
+  return jestPrisma.client
+})
+
 import { TextEncoder, TextDecoder } from 'util'
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
