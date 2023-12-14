37b5cec6df2e5936c31e5458f8a2b799520b605f
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2023-12-14 02:06:39 -0300

    components: refactor Button component

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-const Button = ({ btnType, text, onClick, href, style, className, disabled }) => {
-	const rootClassName = `relative inline-flex items-center justify-center cursor
-pointer no-underline px-3.5 rounded-md font-medium outline-0 select-none align-middle
-whitespace-no-wrap border border-solid bg-tuna-700 border-raven-700 hover:bg-tuna-800
-active:bg-tuna-900`
+const Button = ({ btnType, children, style, onClick, text, className, variant = '', disabled }) => {
+	const rootClassName = cn(
+		'btn',
+		variant,
+		className
+	)
 
 	return (
-		<Link className='no-text-decoration w-full' href={href || ""}>
-			<button type={btnType}
-					className={`text-white shadow-[0_5px_10px_rgb(0,68,255,0.12)] h-10 leading-10 text-[15px] ${rootClassName}`}
-					disabled={disabled}
-					onClick={onClick}>
-				{text && (
-					<span>{text}</span>
-				)}
-			</button>
-		</Link>
+		<button type={btnType}
+				className={rootClassName}
+				disabled={disabled}
+				onClick={onClick} style={style}>
+			{text && (
+				text
+			)}
+
+			{children}
+		</button>
 	)
 }
 
