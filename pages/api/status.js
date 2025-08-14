acca5f3c8dedbbd2229a6df4eeaf05020204998c
Thomas Bergmueller <thomas.bergmueller@authenticvision.com>
2025-08-14 10:39:22 +0200

    Auto-build and publish images
    - Add github actions to build and publish to ghcr.io
    - Bake GIT_COMMIT and GIT_ENV into image
    - add GET api/status endpoint

---
+++ pages/api/status.js
@@ -0,0 +1,15 @@
+import { makePublicEndpoint } from '@/lib/apiHelpers';
+
+const allowedMethods = ['GET']
+
+
+export default async function handle(req, res) {
+	if (!await makePublicEndpoint(req, res, allowedMethods)) return;
+
+    const to_return = {
+        "GIT_COMMIT": process.env.GIT_COMMIT || "notset",
+        "GIT_REF": process.env.GIT_REF || "notset"
+    };	
+
+    return res.status(200).json(to_return)	
+}
